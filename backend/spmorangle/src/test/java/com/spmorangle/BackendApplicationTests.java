893cf4cc966ff08788fcf863d278bd164c27dd3e
eeherngtan <eeherng.tan.2023@scis.smu.edu.sg>
2025-09-17 00:37:15 +0800

    SPMORANGLE-73 fix bug

--- backend/spmorangle/src/test/java/com/spmorangle/BackendApplicationTests.java
+++ backend/spmorangle/src/test/java/com/spmorangle/BackendApplicationTests.java
@@ -1,11 +1,14 @@
 package com.spmorangle;
 
+import com.spmorangle.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestConfig.class)
 class BackendApplicationTests {
 
 	@Test
---
+++ backend/spmorangle/src/test/java/com/spmorangle/config/TestConfig.java
@@ -0,0 +1,33 @@
+package com.spmorangle.config;
+
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+
+@TestConfiguration
+@Profile("test")
+public class TestConfig {
+
+    @Bean
+    @Primary
+    public AwsCredentialsProvider mockAwsCredentialsProvider() {
+        return Mockito.mock(AwsCredentialsProvider.class);
+    }
+
+    @Bean
+    @Primary
+    public CognitoIdentityProviderClient mockCognitoIdentityProviderClient() {
+        return Mockito.mock(CognitoIdentityProviderClient.class);
+    }
+
+    @Bean
+    @Primary
+    public JwtDecoder mockJwtDecoder() {
+        return Mockito.mock(JwtDecoder.class);
+    }
+}
