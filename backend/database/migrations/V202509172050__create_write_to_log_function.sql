49b5b4cbd837094414b102967806abd4ccaa310e
Jordan <jordianojr2001@gmail.com>
2025-09-18 02:53:47 +0800

    debug

--- backend/database/migrations/V202509172050__create_write_to_log_function.sql
+++ backend/database/migrations/V202509172050__create_write_to_log_function.sql
@@ -1,7 +1,7 @@
 CREATE FUNCTION log_update()
 RETURNS TRIGGER AS $$
 BEGIN
-  INSERT INTO syncup.update_logs (type, id, updated_by)
+  INSERT INTO syncup.update_logs (table_type, row_id, updated_by)
   VALUES (TG_TABLE_NAME, NEW.id, NEW.updated_by);
 
   RETURN NEW;
