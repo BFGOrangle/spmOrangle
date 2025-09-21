32eda070bfc635df0fcfc6039e5e8dc6da318958
Chenn02z <yuchen.yeo.2023@scis.smu.edu.sg>
2025-09-21 14:37:25 +0800

    bug fix

--- backend/spmorangle/src/main/java/com/spmorangle/crm/taskmanagement/dto/CreateTaskDto.java
+++ backend/spmorangle/src/main/java/com/spmorangle/crm/taskmanagement/dto/CreateTaskDto.java
@@ -1,7 +1,6 @@
 package com.spmorangle.crm.taskmanagement.dto;
 
 import com.spmorangle.crm.taskmanagement.enums.Status;
-`import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
