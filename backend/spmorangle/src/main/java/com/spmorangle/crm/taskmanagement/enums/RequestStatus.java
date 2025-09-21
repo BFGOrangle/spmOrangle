67aeb99fb8c84a9ec6143a8341f6504a350dc6a4
Chenn02z <yuchen.yeo.2023@scis.smu.edu.sg>
2025-09-21 12:34:18 +0800

    Task Model

---
+++ backend/spmorangle/src/main/java/com/spmorangle/crm/taskmanagement/enums/RequestStatus.java
@@ -0,0 +1,8 @@
+package com.spmorangle.crm.taskmanagement.enums;
+
+public enum RequestStatus {
+    TODO,
+    IN_PROGRESS,
+    COMPLETED,
+    BlOCKED
+}
---
+++ backend/spmorangle/src/main/java/com/spmorangle/crm/taskmanagement/model/Task.java
@@ -0,0 +1,60 @@
+package com.spmorangle.crm.taskmanagement.model;
+
+import com.spmorangle.crm.taskmanagement.enums.RequestStatus;
+import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tasks", schema = "syncup")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @NotNull
+    @Column(name = "project_id", nullable = false)
+    private int projectId;
+
+    @NotNull
+    @Column(name = "owner_id", nullable = false)
+    private int ownerId;
+
+    @Column(name = "task_type", nullable = false)
+    private int taskType;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = true)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private RequestStatus status = RequestStatus.TODO;
+
+    @Column(name = "delete_index", nullable = false)
+    private boolean deleteIndex = false;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @ColumnDefault("now()")
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @ColumnDefault("now()")
+    private OffsetDateTime updatedAt;
+
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private int createdBy;
+
+    @Column(name = "updated_by", nullable = false)
+    private int updatedBy;
+}
