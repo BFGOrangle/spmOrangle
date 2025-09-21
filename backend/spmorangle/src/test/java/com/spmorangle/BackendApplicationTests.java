19154852b5499639bdd8e0cc733faa1efe1887e1
Chenn02z <yuchen.yeo.2023@scis.smu.edu.sg>
2025-09-21 16:37:15 +0800

    test for h2

--- backend/spmorangle/src/test/java/com/spmorangle/BackendApplicationTests.java
+++ backend/spmorangle/src/test/java/com/spmorangle/BackendApplicationTests.java
@@ -6,13 +6,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.defer-datasource-initialization=true",
+    "spring.sql.init.mode=always",
+    "spring.jpa.properties.hibernate.hbm2ddl.create_namespaces=true",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 @ActiveProfiles("test")
 @Import(TestConfig.class)
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
