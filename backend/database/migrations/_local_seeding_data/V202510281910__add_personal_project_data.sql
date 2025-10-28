927c6bda78eeb5b5f18a040db6ecfcceba71dd0b
Mok Cong Yao <164317842+jordianojr@users.noreply.github.com>
2025-10-28 19:39:47 +0800

    local seeding for personal project (#75)

---
+++ backend/database/migrations/_local_seeding_data/V202510281910__add_personal_project_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO syncup.projects (id, name, owner_id, description, delete_ind, created_by, updated_by)
+VALUES
+  (0, 'Personal Task Repository', 3, 'Repository for personal tasks.', false, 3, 3);
